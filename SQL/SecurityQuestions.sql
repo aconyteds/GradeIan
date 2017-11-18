93d95f3fb31db0929923836d7b006649785fec4f
Ian Wilson <ianwilson1337@gmail.com>
2017-11-18 17:08:53 -0600

    Updated available security questions.

---
+++ SQL/SecurityQuestions.sql
@@ -0,0 +1,10 @@
+USE gradeian;
+
+INSERT INTO SecurityQuestions
+VALUES ('Mother\'s Maiden Name');
+
+INSERT INTO SecurityQuestions
+VALUES ('High School Mascot');
+
+INSERT INTO SecurityQuestions
+VALUES ('Street you lived on when you were 10 years old');
