e950cce9c53d19dd828985be636e033a665d79b6
Ian Wilson <ianwilson1337@gmail.com>
2017-11-19 20:52:04 -0600

    Made changes to accommodate evolving requirements.

--- FrontEnd/src/systemjs.config.js
+++ FrontEnd/src/systemjs.config.js
@@ -23,9 +23,12 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+      '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
+      'tslib': 'npm:tslib/tslib.js',
 
       // other libraries
-      'rxjs':                      'npm:rxjs',
+      'rxjs': 'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
       "jquery": "npm:jquery/dist/jquery.min.js"
     },
