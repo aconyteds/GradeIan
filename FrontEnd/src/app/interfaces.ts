488c9c29e241bd6d65b9aadfce62680c64180829
Ian Wilson <ianwilson1337@gmail.com>
2017-11-19 20:49:55 -0600

    Updated User object key names to match Expected paramater values

---
+++ FrontEnd/src/app/interfaces.ts
@@ -0,0 +1,14 @@
+export interface User{
+  firstName:string;
+  lastName:string;
+  email:string;
+  securityQuestion:number;
+  securityAnswer:string;
+  userName:string;
+  password?:string;
+}
+
+export interface SecurityQuestion{
+  ID:number;
+  Question:string;
+}
