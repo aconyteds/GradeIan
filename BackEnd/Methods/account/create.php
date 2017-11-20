a4db03c8971e30ebe6ee7ead4ac352d50324d4e7
Ian Wilson <ianwilson1337@gmail.com>
2017-11-19 20:44:36 -0600

    Updated to account for GET, POST, and payload requests.

--- BackEnd/Methods/account/create.php
+++ BackEnd/Methods/account/create.php
@@ -1,13 +1,42 @@
 <?php
   require "../dbConnect.php";
 
-  $firstName = $_GET["firstName"];
-  $lastName = $_GET["lastName"];
-  $email = $_GET["email"];
-  $securityQuestion = $_GET["securityQuestion"];
-  $securityAnswer = $_GET["securityAnswer"];
-  $userName = $_GET["userName"];
-  $userPassword = $_GET["password"];
+
+
+  if($_GET["userName"]){
+    //handle GET requests
+    $firstName = $_GET["firstName"];
+    $lastName = $_GET["lastName"];
+    $email = $_GET["email"];
+    $securityQuestion = $_GET["securityQuestion"];
+    $securityAnswer = $_GET["securityAnswer"];
+    $userName = $_GET["userName"];
+    $userPassword = $_GET["password"];
+  }
+  else if($_POST["userName"]){
+    //Handle POST requests
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $securityQuestion = $_POST["securityQuestion"];
+    $securityAnswer = $_POST["securityAnswer"];
+    $userName = $_POST["userName"];
+    $userPassword = $_POST["password"];
+  }
+  else{
+    //Handle payload delivery
+    $request_body = file_get_contents('php://input');
+    $data = json_decode($request_body);
+    $firstName = $data->firstName;
+    $lastName = $data->lastName;
+    $email = $data->email;
+    $securityQuestion = $data->securityQuestion;
+    $securityAnswer = $data->securityAnswer;
+    $userName = $data->userName;
+    $userPassword = $data->password;
+
+  }
+
 
   try{
     //Check for unique username
@@ -25,7 +54,7 @@
     }
 
     // Commence with creation of account
-    $response  = $conn->query("call createAccount('$firstName', '$lastName', '$email', '$securityAnswer', $securityQuestion, '$userName', \"$userPassword\")")->fetch(PDO::FETCH_OBJ);
+    $response  = $conn->query("call createAccount('$firstName', '$lastName', '$email', '$securityAnswer', $securityQuestion, '$userName', '$userPassword')")->fetch(PDO::FETCH_OBJ);
     echo json_encode($response);
 
   }catch(PDOException $err){
