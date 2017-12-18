51b85a53a0bea56211e81146e0ea7c051034007a
Ian Wilson <ianwilson1337@gmail.com>
2017-12-18 16:33:16 -0600

    Helper Methods for testing the database procedures.

---
+++ SQL/testInputs.sql
@@ -0,0 +1,27 @@
+use gradeIan;
+
+call createAccount('Ian', 'Wilson', 'ianwilson1337@gmail.com', 'answer', 1, 'iwilson', 'password');
+call createAccount('Clarice', 'Hannibal', 'c@FBI.gov', 'answer', 2, 'cHanny', 'password');
+
+--
+call login('iwilson','');
+-- Successfull Login
+call login('iwilson', 'password');
+-- Failed Login
+call login('iwilson', "abFFF");
+
+call generateToken(1);
+
+call checkEmail("ianwilson1337@gmail.com");
+
+call checkUserName("iwilson");
+
+call authenticate((select token from Tokens where user_id=2));
+
+call createClass("Title", "fa", 1, DATE(NOW()), DATE_ADD(NOW(), INTERVAL 2 MONTH));
+
+Update Tokens set ExpirationDate = NOW() where user_id = 1;
+
+call createStudent(1, "John Doe", "mail@mail.com");
+
+call checkStudentEmail("mail@mail.com");
---
+++ SQL/testOutputs.sql
@@ -0,0 +1,11 @@
+use gradeian;
+
+select * from users;
+
+select * from tokens;
+
+select * from passwords;
+
+select * from classes;
+
+select * from students;
