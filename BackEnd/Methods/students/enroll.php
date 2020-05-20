de4405ae405463795532cea3c2cae57555bf026a
Ian Wilson <ianwilson1337@gmail.com>
2020-05-19 20:52:54 -0500

    Modified methods for enrolling and withdrawing students.

--- BackEnd/Methods/students/enroll.php
+++ BackEnd/Methods/students/enroll.php
@@ -5,10 +5,23 @@
     require_once "../dbConnect.php";
     require_once "../getParameters.php";
     $classId = getParameters(["classId"])->classId;
-    $studentId = getParameters(["studentId"])->studentId;
-    //This will be a student Search
-    $response = $conn->query("call enrollStudent('$classId','$studentId')")->fetch(PDO::FETCH_OBJ);
+    //Explode converts the submission to an array for usage
+    $students = explode(",", getParameters(["students"])->students);
+    try{
+      $enrollResponses->response = [];
+      foreach($students as $id){
+        // Send a request for each student to be enrolled in the class
+        $queryResponse = $conn->query("call enrollStudent('$classId','$id')")->fetch(PDO::FETCH_OBJ);
+        $response = new stdClass();
+        $response->successful = $queryResponse->response;
+        $response->ID = $id;
 
-    echo json_encode($response);
+        array_push($enrollResponses->response, $response);
+      }
+      echo json_encode($enrollResponses);
+    } catch(PDOException $err){
+      echo $err;
+    }
+    $conn=null;
   }
 ?>
--- BackEnd/Methods/students/withdraw.php
+++ BackEnd/Methods/students/withdraw.php
@@ -5,10 +5,23 @@
     require_once "../dbConnect.php";
     require_once "../getParameters.php";
     $classId = getParameters(["classId"])->classId;
-    $studentId = getParameters(["studentId"])->studentId;
-    //This will be a student Search
-    $response = $conn->query("call withdrawStudent('$classId','$studentId')")->fetch(PDO::FETCH_OBJ);
+    //Explode converts the submission to an array for usage
+    $students = explode(",", getParameters(["students"])->students);
+    try{
+      $withdrawResponses->response = [];
+      foreach($students as $id){
+        // Send a request for each student to be enrolled in the class
+        $queryResponse = $conn->query("call withdrawStudent('$classId','$id')")->fetch(PDO::FETCH_OBJ);
+        $response = new stdClass();
+        $response->successful = $queryResponse->response;
+        $response->ID = $id;
 
-    echo json_encode($response);
+        array_push($withdrawResponses->response, $response);
+      }
+      echo json_encode($withdrawResponses->response);
+    } catch(PDOException $err){
+      echo $err;
+    }
+    $conn=null;
   }
 ?>
