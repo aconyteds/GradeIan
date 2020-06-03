4fd9cfd2a091ba77c158b0c42e901e467bc816a3
Ian Wilson <ianwilson1337@gmail.com>
2020-06-02 20:47:44 -0500

    Modified the save method so that when called, all assignments and their items are sent to the DB.

--- FrontEnd/src/app/components/assignments/assignmentView.component.ts
+++ FrontEnd/src/app/components/assignments/assignmentView.component.ts
@@ -1,10 +1,13 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { NgClass } from "@angular/common";
-import {NgForm, PatternValidator, EmailValidator} from "@angular/forms";
+import { NgForm } from "@angular/forms";
+import { Observable, forkJoin } from "rxjs";
 
-import { AssignmentGroupModel, AssignmentItemModel } from "./assignmentModel";
+import { AssignmentGroupModel } from "./assignmentModel";
 
 import { AssignmentService } from "../../services/assignment.service";
+
+import { AssignmentList } from "./assignmentList.component";
 
 @Component({
   selector: 'assignment-view',
@@ -14,15 +17,22 @@ import { AssignmentService } from "../../services/assignment.service";
         margin-bottom:10px;
     }
 
-    .assignment-item{
+    .assignment-group{
       font-size:20px;
+      border-bottom:1px solid #ccc;
+      padding-top:10px;
+    }
+
+    .assignment-group:nth-child(even){
+      background:#FAFAFA;
     }
     `],
   templateUrl: "./assignmentView.template.html"
 })
 
 export class AssignmentView {
-  public invalidEmail = false;
+  @ViewChildren(AssignmentList)
+  public assignmentItems: QueryList<AssignmentList>;
   public assignmentData: AssignmentGroupModel[] = [];
   public totalWeight = 0;
   constructor(
@@ -33,7 +43,7 @@ export class AssignmentView {
   public addAssignment() {
     const tempWeight = this.totalWeight < 100 ? (100 - this.totalWeight) : 1;
     this.assignmentData.push(new AssignmentGroupModel(0,
-      "Assignment " + (this.assignmentData.length + 1).toString(),
+      "Assignment Group",
       tempWeight));
     this.calculateTotalWeight();
   }
@@ -47,9 +57,37 @@ export class AssignmentView {
     this.totalWeight = currWeight;
   }
 
-  public createAssignment(classId: number) {
+  public saveAssignments(classId: number): Observable<any> {
     // Method called by parent to create assignments
-    // this.assignmentService.createAssignment(this.assignmentData);
+    const assignments = this.assignmentItems.toArray();
+    console.log(assignments);
+    // Iterate through the child assignments to get the items
+    const assignmentItems = this.assignmentData.map((item, indx) => {
+      // Set the Class ID for the current Assignment Group Item
+      item.classId = classId;
+      // Attempt to get assignments for the Current assignemt Group
+      let currItem = assignments[indx];
+      // Do a check to make sure that this is the correct assignment group
+      if (currItem.defaultName !== item.title) {
+        // Not the right group, find the right one
+        assignments.some((assignmentItem) => {
+          if (assignmentItem.defaultName === item.title) {
+            // Right group found, associate and exit out of the array
+            currItem = assignmentItem;
+            return true;
+          }
+        });
+      }
+      return currItem.assignmentData;
+    });
+
+    const currRequest = this.assignmentService.createAssignment(this.assignmentData, assignmentItems);
+    currRequest.subscribe((response) => {
+      console.log(response);
+      // return assignmentItems[indx].saveAssignmentItems(response.response);
+    });
+
+    return currRequest;
   }
 
   public removeAssignment(assignment: AssignmentGroupModel) {
