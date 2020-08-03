68ab3ecd9ee3a9153cbc8ec211531487dd2819ac
Ian Wilson <ianwilson1337@gmail.com>
2020-08-03 14:54:17 -0500

    Modified the date to properly reflect the local time.

--- FrontEnd/src/app/components/admin/adminMenu.component.ts
+++ FrontEnd/src/app/components/admin/adminMenu.component.ts
@@ -67,7 +67,7 @@ export class AdminMenuComponent implements OnInit {
     const d = new Date();
     const months = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"];
-    return months[d.getMonth()] + " " + d.getUTCDate() + ", " + d.getFullYear();
+    return months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
   }
 
   public updateAccount(event: Event) {
