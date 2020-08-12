ce095fc0c92c0d6c8ba8d622f2f1b855508a8ff0
Ian Wilson <ianwilson1337@gmail.com>
2020-08-12 08:55:24 -0500

    Added pages for troubleshooting deployments in hosting environments.

---
+++ BackEnd/ipageDeployments/authenticate.php
@@ -0,0 +1,19 @@
+<?php
+  require realpath(__DIR__."/getParameters.php");
+  require realpath(__DIR__."/dbConnect.php");
+  require realpath(__DIR__."/getUserIP.php");
+
+  $token = getParameters(["token"])->token;
+  if($token){
+    $userId = $conn->query("call authenticate('$token', '$user_ip')")->fetch(PDO::FETCH_OBJ)->UserID;
+    if(!$userId){
+      http_response_code(401);
+    }
+    else{
+      //echo json_encode(array("userId"=>$UserId));
+    }
+  }
+  else{
+    http_response_code(401);
+  }
+ ?>
---
+++ BackEnd/ipageDeployments/include.php
@@ -0,0 +1,4 @@
+<?php
+  echo realpath(__DIR__."/getUserIP.php");
+  require realpath(__DIR__."/getUserIP.php");
+?>
